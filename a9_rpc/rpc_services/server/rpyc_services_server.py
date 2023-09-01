7c087684738562b5cade28fda23efa6164f31ac7
Newton Miyoshi <newton.miyoshi@medway.com.br>
2023-09-01 16:11:16 -0300

    a9 rpc

---
+++ a9_rpc/rpc_services/server/rpyc_services_server.py
@@ -0,0 +1,17 @@
+import rpyc
+
+class CalculadoraService(rpyc.Service):
+    def exposed_soma(self, a, b):
+        return a + b
+    def exposed_subtracao(self, a, b):
+        return a - b
+    def exposed_multiplicacao(self, a, b):
+        return a * b
+    def exposed_divisao(self, a, b):
+        return a / b
+    def exposed_teste(self):
+        print("Oi! Testando...")
+
+if __name__ == "__main__":
+    server = rpyc.ThreadedServer(CalculadoraService, port = 18811)
+    server.start()
